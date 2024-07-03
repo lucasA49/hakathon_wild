5a8fd79a2936ec6db9fd1b16495f913f0890623a
lucasA49 <lucasaksu@gmail.com>
2024-07-03 23:49:37 +0200

    add buton in the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,23 @@
+import { NavLink } from "react-router-dom";
 import BubbleChat from "../assets/icone/bubble-chat.png";
 import Calendar from "../assets/icone/calendar-2.png";
 import Home from "../assets/icone/home-3.png";
 
+
 export default function Footer() {
   return (
     <footer className="footer-container">
-      <img className="calendar" src={Calendar} alt="" />
-      <img className="home" src={Home} alt="" />
-      <img className="chat" src={BubbleChat} alt="" />
+      <NavLink className="navCalendar" to="/contact">
+        <img className="calendar" src={Calendar} alt="" />
+      </NavLink>
+
+      <NavLink className="navHome" to="/home">
+        <img className="home" src={Home} alt="" />
+      </NavLink>
+
+      <NavLink className="navChat" to="/discussion">
+        <img className="chat" src={BubbleChat} alt="" />
+      </NavLink>
     </footer>
   );
 }
