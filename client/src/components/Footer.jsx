259c50fbcce8f73cca05c535322dfcd24a9199fb
Lucasa49 <lucasaksu@gmail.com>
2024-07-03 17:52:54 +0200

    add the footer bar and the picture in the nav

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import BubbleChat from "../assets/icone/bubble-chat.png";
+import Calendar from "../assets/icone/calendar-2.png";
+import Home from "../assets/icone/home-3.png";
+
 export default function Footer() {
   return (
-    <div className="footer-container">
-      <p>Composants Footer</p>
-    </div>
+    <footer className="footer-container">
+      <img className="calendar" src={Calendar} alt="" />
+      <img className="home" src={Home} alt="" />
+      <img className="chat" src={BubbleChat} alt="" />
+    </footer>
   );
 }
