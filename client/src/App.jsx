4e2fbf3364dadc86feb5aabb52c449d1bd30a731
AurelienChetot <aurelien.chetot@gmail.com>
2024-07-03 14:36:05 +0200

    feat: init route and scss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import "./App.css";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Import du fichier App.scss
+import "./App.scss";
 
 function App() {
   return (
-  <>
-  </>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   );
 }
 
