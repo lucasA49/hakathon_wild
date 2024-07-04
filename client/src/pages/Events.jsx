a8038aa62cca450ee1e812b808132894084ed5ad
ATN <contact.atn35@gmail.com>
2024-07-04 17:07:32 +0200

    conflict resolution

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,36 +1,36 @@
-// import '../style/Events.scss';
+import '../style/Events.scss';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header>
-//         <h1>Événements à venir</h1>
-//         <input type="text" placeholder="Rechercher un événement..." />
-//       </header>
-//       <main>
-//         <div className="event-cards">
-//           {events.map((event) => (
-//             <div className="event-card" key={event.id}>
-//               <img src="" alt="" />
-//               <h2>{event.title}</h2>
-//               <p>{event.description}</p>
-//               <button type='button'>inscription</button>
-//             </div>
-//           ))}
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Événements à venir</h1>
+        <input type="text" placeholder="Rechercher un événement..." />
+      </header>
+      <main>
+        <div className="event-cards">
+          {events.map((event) => (
+            <div className="event-card" key={event.id}>
+              <img src="" alt="" />
+              <h2>{event.title}</h2>
+              <p>{event.description}</p>
+              <button type='button'>inscription</button>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
 
-// const events = [
-//   { id: 1, title: "Foot en salle", description: "3 Septembre 2024 à 20:00 Footcenter PARIS XV" },
-//   { id: 2, title: "Afterwork ", description: "22 Septembre 2024 à 20:00 La vilette" },
-//   { id: 3, title: "Crèche family", description: "5 Septembre 2024 à 13:00" },
-//   { id: 4, title: "Atelier et conférence", description: "25 Octobre 2024 à 19:00 Saint-germain des près" },
-//   { id: 5, title: "Conduite", description: "Du lundi au vendredi" },
-//   { id: 6, title: "Bowling ", description: "12 Octobre à 21:00 Paris XIIème" },
+const events = [
+  { id: 1, title: "Foot en salle", description: "3 Septembre 2024 à 20:00 Footcenter PARIS XV" },
+  { id: 2, title: "Afterwork ", description: "22 Septembre 2024 à 20:00 La vilette" },
+  { id: 3, title: "Crèche family", description: "5 Septembre 2024 à 13:00" },
+  { id: 4, title: "Atelier et conférence", description: "25 Octobre 2024 à 19:00 Saint-germain des près" },
+  { id: 5, title: "Co-voiturage", description: "Du lundi au vendredi" },
+  { id: 6, title: "Théatre ", description: "12 Octobre à 21:00 Paris XIIème" },
 
-// ];
+];
 
-// export default App;
+export default App;
