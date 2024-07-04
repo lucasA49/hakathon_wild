ffd0ff1de1c95b0dad5f226110a07694946f44f3
derimer <jderimer@gmail.com>
2024-07-04 11:32:02 +0200

    debut de la page d accueil

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,10 +7,13 @@ export default function Home() {
     <div className="accueil-container">
       <img id="uniwork" src={uniwork} alt="logo" />
       <h1>Bienvenue "pseudo"</h1>
+
+      <h2>Voici quelques articles qui peuvent t 'interesser</h2>
       <article>
-        <h2>Voici quelques articles qui peuvent t 'interesser</h2>
-        <h3>la discrimination au travail</h3>
-        <img src={inclusivity} alt="inclusivity" />
+        <h4>la discrimination au travail</h4>
+
+        <img id="inclusivity" src={inclusivity} alt="inclusivity" />
+        <button type="button">lire</button>
       </article>
     </div>
   );
