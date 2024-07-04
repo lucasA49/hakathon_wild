87ce63e2899b51e1592793a864e64a26f6a6dd0f
AurelienChetot <aurelien.chetot@gmail.com>
2024-07-04 16:11:06 +0200

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -23,6 +23,7 @@ const router = createBrowserRouter([
         path: "/ressources",
         element: <Ressources />,
       },
+      {
         path: "/events",
         element: <Events />,
       },
