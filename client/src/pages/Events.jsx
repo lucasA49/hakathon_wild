62c93f0bff357858730258a4b56fa434b957ebd0
ATN <contact.atn35@gmail.com>
2024-07-05 09:44:35 +0200

    page events ends

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,20 +1,79 @@
-import '../style/Events.scss';
+import "../style/Events.scss";
+import foot from "../assets/images/foot-events.png";
+import cocktail from "../assets/images/cocktail-events.png";
+import child from "../assets/images/enfant-events.png";
+import conference from "../assets/images/conference-events.png";
+import car from "../assets/images/voiture-events.png";
+import bowling from "../assets/images/bowling-events.png";
+import searchIcon from "../assets/images/Search.svg";
 
-function App() {
+export default function Events() {
+  const events = [
+    {
+      id: 1,
+      title: "Foot en salle",
+      description: "3 Septembre 2024 à 20:00 Footcenter PARIS XV",
+      image: foot,
+    },
+    {
+      id: 2,
+      title: "Afterwork",
+      description: "22 Septembre 2024 à 20:00 La Villette",
+      image: cocktail,
+    },
+    {
+      id: 3,
+      title: "Crèche family",
+      description: "5 Septembre 2024 à 13:00",
+      image: child,
+    },
+    {
+      id: 4,
+      title: "Atelier et conférence",
+      description: "25 Octobre 2024 à 19:00 Saint-Germain des Prés",
+      image: conference,
+    },
+    {
+      id: 5,
+      title: "Co-voiturage",
+      description: "Du lundi au vendredi",
+      image: car,
+    },
+    {
+      id: 6,
+      title: "Théâtre",
+      description: "12 Octobre à 21:00 Paris XIIème",
+      image: bowling,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header>
-        <h1>Événements à venir</h1>
-        <input type="text" placeholder="Rechercher un événement..." />
+    <div className="Events">
+      <header className="header-events">
+        <h1 className="h1-events">Évènements à venir</h1>
+        <div className="search-bar-events">
+          <input
+            className="input-events"
+            type="text"
+            placeholder="Recherche..."
+          />
+          <img
+            src={searchIcon}
+            alt="Icône de recherche"
+            className="search-icon"
+          />
+        </div>
       </header>
-      <main>
+      <main className="main-events">
         <div className="event-cards">
           {events.map((event) => (
             <div className="event-card" key={event.id}>
-              <img src="" alt="" />
-              <h2>{event.title}</h2>
-              <p>{event.description}</p>
-              <button type='button'>inscription</button>
+              <img className="img-events" src={event.image} alt={event.title} />
+              <h2 className="h2-events">{event.title}</h2>
+              <p className="p-events">{event.description}</p>
+              <button className="button-events" type="button">
+                Inscription
+              </button>
             </div>
           ))}
         </div>
@@ -22,15 +81,3 @@ function App() {
     </div>
   );
 }
-
-const events = [
-  { id: 1, title: "Foot en salle", description: "3 Septembre 2024 à 20:00 Footcenter PARIS XV" },
-  { id: 2, title: "Afterwork ", description: "22 Septembre 2024 à 20:00 La vilette" },
-  { id: 3, title: "Crèche family", description: "5 Septembre 2024 à 13:00" },
-  { id: 4, title: "Atelier et conférence", description: "25 Octobre 2024 à 19:00 Saint-germain des près" },
-  { id: 5, title: "Co-voiturage", description: "Du lundi au vendredi" },
-  { id: 6, title: "Théatre ", description: "12 Octobre à 21:00 Paris XIIème" },
-
-];
-
-export default App;
